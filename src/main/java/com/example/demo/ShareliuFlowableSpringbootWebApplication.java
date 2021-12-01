4b980193996a6687eb70736d47c2ce4cce67d4c4
wenjunxp@sina.com <wenjunxp@sina.com>
2021-12-01 22:54:54 +0800

    P49

---
+++ src/main/java/com/example/demo/ShareliuFlowableSpringbootWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareliuFlowableSpringbootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareliuFlowableSpringbootWebApplication.class, args);
+	}
+
+}
